bde766fe96e8850d44a2d6ca4e84d59628d37ea2
Alejandro <ale22alala2@gmail.com>
2025-07-15 11:19:31 +0200

    Boton entre interfaces + pequeños detalles de decoración

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) #Crea la aplicación flask.
 
-@app.route('/')
+@app.route('/') #Muestra el interfaz de index.html como el principal al abrir la web.
 def home():
 	return render_template('index.html')
 
-if __name__=='__main__':
+@app.route("/formulario") #Crea la ruta para acceder a la pagina del formulario de nuevas recetas.
+def formulario():
+	return render_template("formulario.html")
+
+if __name__=='__main__': #Enciende el servidor.
 	app.run(debug=True)
 
